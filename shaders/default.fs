b86657d847928c5ab7b9501168628f4f538b2850
Jan Kuhlmann <33833587+M2-TE@users.noreply.github.com>
2023-11-20 15:02:57 +0100

    material properties

--- shaders/default.fs
+++ shaders/default.fs
@@ -11,6 +11,22 @@ out vec4 pixelColor;
 uniform sampler2D textureSampler;
 layout (location = 16) uniform vec3 cameraWorldPos;
 
+// first define custom struct
+struct Material {
+    vec3 ambient; // first slot
+    vec3 diffuse; // second slot
+    vec3 specular; // third slot
+    float shininess; // fourth slot
+};
+
+// Material needs 4 slots, similar to a matrix
+layout (location = 17) uniform Material material = {
+    vec3(0.1f),
+    vec3(1.0f),
+    vec3(1.0f),
+    32.0f
+};
+
 void main() {
     // create our "sun"
     vec3 lightColor = vec3(.992f, .984f, .827f);
@@ -18,21 +34,20 @@ void main() {
     vec3 lightDir = normalize(lightWorldPos - worldPos); // unit vector from light to fragment
 
     // ambient color (low light from scattered sunlight)
-    float ambientStrength = 0.1f; // arbitrary setting
-    vec3 ambientColor = lightColor * ambientStrength;
+    float ambientStrength = 1.0f; // ambient modifier
+    vec3 ambientColor = lightColor * ambientStrength * material.ambient;
 
     // light color
     float diffuseStrength = dot(normal, lightDir); // calc intensity of light
     diffuseStrength = max(diffuseStrength, 0.0f); // filter out negative intensity
-    vec3 diffuseColor = lightColor * diffuseStrength;
+    vec3 diffuseColor = lightColor * diffuseStrength * material.diffuse;
 
     // specular color
     vec3 cameraDir = normalize(cameraWorldPos - worldPos); // unit vector from camera to fragment
     vec3 reflectDir = reflect(-lightDir, normal);
-    float specularStrength = 0.5f; // initial strength (0.0f to 1.0f)
-    float shininess = 32.0f; // controls size/intensity of specular highlight
-    specularStrength *= pow(max(dot(cameraDir, reflectDir), 0.0), shininess);
-    vec3 specularColor = lightColor * specularStrength;
+    float specularStrength = 0.5f; // specular modifier
+    specularStrength *= pow(max(dot(cameraDir, reflectDir), 0.0), material.shininess);
+    vec3 specularColor = lightColor * specularStrength * material.specular;
 
     // final color
     vec3 color = texture(textureSampler, uvCoord).rgb; // sample texture
