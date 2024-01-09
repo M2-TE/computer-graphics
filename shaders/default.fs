846c4ca601242a823b592aba13edd7e28cf2ec40
Jan Kuhlmann <33833587+M2-TE@users.noreply.github.com>
2024-01-09 13:18:53 +0100

    cleaned up fragment shader

--- shaders/default.fs
+++ shaders/default.fs
@@ -30,58 +30,63 @@ layout (location = 35) uniform float lightFar;
 layout (binding = 0) uniform sampler2D diffuseTexture;
 layout (binding = 1) uniform samplerCube shadowMap;
 
-void main() {
-    // create our "sun"
-    vec3 lightDir = normalize(lightWorldPos - worldPos); // unit vector from light to fragment
-
-    // ambient color (low light from scattered sunlight)
+vec3 calc_ambient() {
     float ambientStrength = 1.0; // ambient modifier
-    vec3 ambientColor = lightColor * ambientStrength * material.ambient;
-
-    // light color
+    return lightColor * ambientStrength * material.ambient;
+}
+vec3 calc_diffuse() {
+    vec3 lightDir = normalize(lightWorldPos - worldPos); // unit vector from light to fragment
     float diffuseStrength = dot(normal, lightDir); // calc intensity of light
     diffuseStrength = max(diffuseStrength, 0.0); // filter out negative intensity
-    vec3 diffuseColor = lightColor * diffuseStrength * material.diffuse;
-
-    // specular color
+    return lightColor * diffuseStrength * material.diffuse;
+}
+vec3 calc_specular() {
+    vec3 lightDir = normalize(lightWorldPos - worldPos); // unit vector from light to fragment
     vec3 cameraDir = normalize(cameraWorldPos - worldPos); // unit vector from camera to fragment
     vec3 reflectDir = reflect(-lightDir, normal);
     float specularStrength = material.shininessStrength; // specular modifier
     specularStrength *= pow(max(dot(cameraDir, reflectDir), 0.0), material.shininess);
-    vec3 specularColor = lightColor * specularStrength * material.specular;
-
-    // shadow color
+    return lightColor * specularStrength * material.specular;
+}
+float calc_shadow() {
+    vec3 lightDir = normalize(lightWorldPos - worldPos); // unit vector from light to fragment
     vec3 fragToLight = worldPos - lightWorldPos;
-    float closestDepth = texture(shadowMap, fragToLight).r; // depth as seen from light
-    closestDepth *= lightFar;
-    float currentDepth = length(fragToLight);
     float bias = max(0.5 * (1.0 - dot(normal, lightDir)), 0.005);  
-    float shadow = currentDepth - bias > closestDepth ? 0.0 : 1.0;
+    // float closestDepth = texture(shadowMap, fragToLight).r; // depth as seen from light
+    // closestDepth *= lightFar;
+    // float currentDepth = length(fragToLight);
+    // float shadow = currentDepth - bias > closestDepth ? 0.0 : 1.0;
     
     // percentage closer filter
     float samples = 4.0;
     float offset  = 0.1;
-    for(float x = -offset; x < offset; x += offset / (samples * 0.5))
-    {
-        for(float y = -offset; y < offset; y += offset / (samples * 0.5))
-        {
-            for(float z = -offset; z < offset; z += offset / (samples * 0.5))
-            {
+    float shadow = 0.0;
+    for(float x = -offset; x < offset; x += offset / (samples * 0.5)) {
+        for(float y = -offset; y < offset; y += offset / (samples * 0.5)) {
+            for(float z = -offset; z < offset; z += offset / (samples * 0.5)) {
                 float closestDepth = texture(shadowMap, fragToLight + vec3(x, y, z)).r; 
-                closestDepth *= lightFar;   // undo mapping [0;1]
+                closestDepth *= lightFar;
                 if(currentDepth - bias < closestDepth) shadow += 1.0;
             }
         }
     }
-    shadow /= (samples * samples * samples);
+    return shadow / (samples * samples * samples);
+}
+vec4 calc_light() {
+    vec3 ambientColor = calc_ambient(); // indirect scattered light
+    vec3 diffuseColor = calc_diffuse(); // direct light
+    vec3 specularColor = calc_specular(); // specular highlights (from direct light)
+    float shadow = calc_shadow(); // shadow influence
 
-    // final color (blend/interpolate vertex color with texture color)
+    // blend texture/vertex colors
     vec4 sampledColor = texture(diffuseTexture, uvCoord);
     vec4 color = mix(vertCol, sampledColor, material.diffuseBlend);
-    // combine with light colors
-    color = vec4(color.rgb * (ambientColor + shadow * (diffuseColor + specularColor)), color.a);
 
-    // write to screen
-    pixelColor = color;
+    // blend sampled color with light
+    return vec4(color.rgb * (ambientColor + shadow * (diffuseColor + specularColor)), color.a);
+}
+
+void main() {
+    pixelColor = calc_light();
     // pixelColor = vec4(vec3(closestDepth / 50.0), 1.0);
 }
