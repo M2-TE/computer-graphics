a48b3ad0c8dcfc576a972eae1f02c1f3e44aed2a
Jan Kuhlmann <33833587+M2-TE@users.noreply.github.com>
2024-10-16 13:14:26 +0200

    triangle

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,26 @@
-#include "app.hpp"
+#define SDL_MAIN_USE_CALLBACKS
+#include <SDL3/SDL_main.h>
+#include "engine.hpp"
 
-// enforce optimus usage on hybrid GPU systems (integrated + nvidia)
-#ifdef _WIN32
-#include <windows.h>
-extern "C" {
-    _declspec(dllexport) DWORD NvOptimusEnablement = 0x00000001;
+SDL_AppResult SDL_AppInit(void** appstate_pp, int argc, char** argv) {
+    // create new engine object and put it into the SDL appstate
+    Engine* engine_p = new Engine();
+    *appstate_pp = engine_p;
+    // init engine and return success
+    engine_p->init();
+    return SDL_AppResult::SDL_APP_CONTINUE;
 }
-#endif
-
-int main() {
-    App app;
-    return app.run();
+SDL_AppResult SDL_AppEvent(void* appstate_p, SDL_Event* event_p) {
+    Engine* engine_p = (Engine*)(appstate_p);
+    return engine_p->execute_event(event_p);
+}
+SDL_AppResult SDL_AppIterate(void* appstate_p) {
+    Engine* engine_p = (Engine*)(appstate_p);
+    engine_p->execute_frame();
+    return SDL_AppResult::SDL_APP_CONTINUE;
+}
+void SDL_AppQuit(void* appstate_p, SDL_AppResult) {
+    Engine* engine_p = (Engine*)(appstate_p);
+    engine_p->destroy();
+    delete engine_p;
 }
